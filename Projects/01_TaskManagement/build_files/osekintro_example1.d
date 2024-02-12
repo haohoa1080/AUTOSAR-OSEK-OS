ce35a3bcd997e6b62d74eff87b618af440253af2
Banh Hao <haohoa1080@gmail.com>
2024-02-12 13:28:50 +0700

    modify name 01_TaskManagement

--- Projects/01_TaskManagement/build_files/osekintro_example1.d
+++
@@ -1,4 +0,0 @@
-./build_files/osekintro_example1.o: OsekIntro_Example1.c \
-  ..\..\OsekLib\os.h ..\..\oil\OsCfg.h ..\..\OsekLib\OsTypes.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
