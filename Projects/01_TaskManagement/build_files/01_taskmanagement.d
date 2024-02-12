cd965460582302f3d1455c7c51e59e70905e429b
Banh Hao <haohoa1080@gmail.com>
2024-02-12 13:24:06 +0700

    add 01_TaskManagement project

---
+++ Projects/01_TaskManagement/build_files/01_taskmanagement.d
@@ -0,0 +1,4 @@
+./build_files/01_taskmanagement.o: 01_TaskManagement.c ..\..\OsekLib\os.h \
+  ..\01_TaskManagement\OsCfg.h ..\..\OsekLib\OsTypes.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ Projects/01_TaskManagement/build_files/oscfg.d
@@ -0,0 +1,4 @@
+./build_files/oscfg.o: OsCfg.c ..\01_TaskManagement\OsCfg.h \
+  ..\..\OsekLib\OsTypes.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
